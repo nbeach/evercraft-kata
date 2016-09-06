94924cc4bf905caea2fd0712f3f2b0ae8fd4e135
Nicholas Beach <nicholascbeach@gmail.com>
2016-09-06 19:50:58 -0400

    Added Ability has modifiers that vary by score test

--- src/main/java/com/nicholasbeach/evercraft/Ability.java
+++ src/main/java/com/nicholasbeach/evercraft/Ability.java
@@ -2,9 +2,37 @@ package com.nicholasbeach.evercraft;
 
 import com.nicholasbeach.evercraft.exception.ScoreOutOfBoundsException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ability {
 
-    int score;
+    private int score;
+    private Map<Integer, Integer> scoreToModifierMap;
+
+    public Ability() {
+        scoreToModifierMap = new HashMap<>();
+        scoreToModifierMap.put(1, -5);
+        scoreToModifierMap.put(2, -4);
+        scoreToModifierMap.put(3, -4);
+        scoreToModifierMap.put(4, -3);
+        scoreToModifierMap.put(5, -3);
+        scoreToModifierMap.put(6, -2);
+        scoreToModifierMap.put(7, -2);
+        scoreToModifierMap.put(8, -1);
+        scoreToModifierMap.put(9, -1);
+        scoreToModifierMap.put(10, 0);
+        scoreToModifierMap.put(11, 0);
+        scoreToModifierMap.put(12, 1);
+        scoreToModifierMap.put(13, 1);
+        scoreToModifierMap.put(14, 2);
+        scoreToModifierMap.put(15, 2);
+        scoreToModifierMap.put(16, 3);
+        scoreToModifierMap.put(17, 3);
+        scoreToModifierMap.put(18, 4);
+        scoreToModifierMap.put(19, 4);
+        scoreToModifierMap.put(20, 5);
+    }
 
     public int getScore() {
         return score;
@@ -15,5 +43,9 @@ public class Ability {
             throw new ScoreOutOfBoundsException("The score " + score + " is outside of the allowed range of 1-20");
         }
         this.score = score;
+    }
+
+    public Integer getModifier() {
+        return scoreToModifierMap.get(score);
     }
 }
--- src/test/java/com/nicholasbeach/evercraft/AbilityTest.java
+++ src/test/java/com/nicholasbeach/evercraft/AbilityTest.java
@@ -4,6 +4,9 @@ import com.nicholasbeach.evercraft.exception.ScoreOutOfBoundsException;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AbilityTest {
@@ -38,5 +41,38 @@ public class AbilityTest {
     @Test(expected = ScoreOutOfBoundsException.class)
     public void whenGivenAScore_GreaterThan20_ThrowsAnOutOfBoundsException() {
         ability.setScore(21);
+    }
+
+    @Test
+    public void hasAModifierThatVariesByScore() {
+        Map<Integer, Integer> scoreToModifierMap = new HashMap<>();
+        scoreToModifierMap.put(1, -5);
+        scoreToModifierMap.put(2, -4);
+        scoreToModifierMap.put(3, -4);
+        scoreToModifierMap.put(4, -3);
+        scoreToModifierMap.put(5, -3);
+        scoreToModifierMap.put(6, -2);
+        scoreToModifierMap.put(7, -2);
+        scoreToModifierMap.put(8, -1);
+        scoreToModifierMap.put(9, -1);
+        scoreToModifierMap.put(10, 0);
+        scoreToModifierMap.put(11, 0);
+        scoreToModifierMap.put(12, 1);
+        scoreToModifierMap.put(13, 1);
+        scoreToModifierMap.put(14, 2);
+        scoreToModifierMap.put(15, 2);
+        scoreToModifierMap.put(16, 3);
+        scoreToModifierMap.put(17, 3);
+        scoreToModifierMap.put(18, 4);
+        scoreToModifierMap.put(19, 4);
+        scoreToModifierMap.put(20, 5);
+
+        for(Integer score : scoreToModifierMap.keySet()) {
+            Ability ability = new Ability();
+            ability.setScore(score);
+
+            assertThat(ability.getModifier()).isEqualTo(scoreToModifierMap.get(score));
+        }
+
     }
 }
