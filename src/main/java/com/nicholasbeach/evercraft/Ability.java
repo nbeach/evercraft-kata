dd78d71c37abb2ac0b664f17d690ea4308257f72
Nicholas Beach <nicholascbeach@gmail.com>
2016-09-06 19:32:07 -0400

    Added Ability when given a score greater than 20 throws an exception test

--- src/main/java/com/nicholasbeach/evercraft/Ability.java
+++ src/main/java/com/nicholasbeach/evercraft/Ability.java
@@ -9,7 +9,7 @@ public class Ability {
     }
 
     public void setScore(int score) {
-        if(score < 1) {
+        if(score < 1 || score > 20) {
             throw new ScoreOutOfBoundsException("The score " + score + " is outside of the allowed range of 1-20");
         }
         this.score = score;
--- src/test/java/com/nicholasbeach/evercraft/AbilityTest.java
+++ src/test/java/com/nicholasbeach/evercraft/AbilityTest.java
@@ -1,22 +1,33 @@
 package com.nicholasbeach.evercraft;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AbilityTest {
 
+    private Ability ability;
+
+    @Before
+    public void before() {
+        ability = new Ability();
+    }
+
     @Test
     public void canBeGivenAScore() {
         int score = 5;
-        Ability ability = new Ability();
         ability.setScore(score);
         assertThat(ability.getScore()).isEqualTo(score);
     }
 
     @Test(expected = ScoreOutOfBoundsException.class)
     public void whenGivenAScore_LessThan1_ThrowsAnOutOfBoundsException() {
-        Ability ability = new Ability();
         ability.setScore(0);
+    }
+
+    @Test(expected = ScoreOutOfBoundsException.class)
+    public void whenGivenAScore_GreaterThan20_ThrowsAnOutOfBoundsException() {
+        ability.setScore(21);
     }
 }
